6838f20bf686e14f057904d53e08084a1d1fd634
Mariia_Ostapets <mariyaostapets@gmail.com>
2024-08-02 13:09:39 +0300

    Perform homework 12

--- src/main.js
+++ src/main.js
@@ -20,11 +20,11 @@ loadMoreBtn.addEventListener('click', onLoadMore);
 
 async function onSearch(event) {
   event.preventDefault();
+
   const query = event.target.elements.query.value.trim();
 
   if (query === '') {
-    iziToast.error({
-      title: 'Error',
+    iziToast.info({
       message: 'Search query cannot be empty!',
       position: 'center',
     });
@@ -33,33 +33,34 @@ async function onSearch(event) {
 
   gallery.innerHTML = '';
   loaderTop.classList.remove('hidden');
+  loadMoreBtn.classList.add('hidden');
   page = 1;
 
   try {
     const data = await fetchGallery(query, page);
-
     loaderTop.classList.add('hidden');
-    loaderBottom.classList.add('hidden');
 
     if (data.hits.length === 0) {
-      iziToast.error({
-        title: 'Error',
+      iziToast.info({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
         position: 'center',
       });
-      loadMoreBtn.classList.add('hidden');
       return;
     }
 
     renderGallery(data.hits);
+
     simpleLightbox = new SimpleLightbox('.gallery a');
     simpleLightbox.refresh();
+
     loadMoreBtn.classList.remove('hidden');
+
     page += 1;
     currentQuery = query;
   } catch (error) {
     loaderTop.classList.add('hidden');
+
     iziToast.error({
       title: 'Error',
       message: error.message,
@@ -72,23 +73,29 @@ async function onLoadMore(event) {
   event.preventDefault();
   loaderBottom.classList.remove('hidden');
   loadMoreBtn.classList.add('hidden');
+
   try {
     const data = await fetchGallery(currentQuery, page);
+
     renderGallery(data.hits);
+
     simpleLightbox = new SimpleLightbox('.gallery a');
     simpleLightbox.refresh();
+
     page += 1;
+
     if (page * 15 >= data.totalHits) {
       loadMoreBtn.classList.add('hidden');
-      iziToast.error({
-        title: 'Error',
+      iziToast.info({
         message: "We're sorry, but you've reached the end of search results.",
-        position: 'center',
+        position: 'topRight',
       });
     } else {
       loadMoreBtn.classList.remove('hidden');
     }
+
     loaderBottom.classList.add('hidden');
+
     smoothScroll();
   } catch (error) {
     loaderBottom.classList.add('hidden');
@@ -101,9 +108,12 @@ async function onLoadMore(event) {
 }
 
 function smoothScroll() {
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
+  const firstCard = gallery.firstElementChild;
+
+  if (!firstCard) return;
+
+  const firstCardRect = firstCard.getBoundingClientRect();
+  const cardHeight = firstCardRect.height;
 
   window.scrollBy({
     top: cardHeight * 2,
